de2026063db6d05a5b2330c13c58381347c64297
Udaysai <udaysai.kandregula@gmail.com>
2025-11-21 08:32:29 +0530

    Area of a circle using (3.14*r*r)

---
+++ Area_Circle.cpp
@@ -0,0 +1,11 @@
+//Finding the area of a circle when input r is given by the use
+
+#include<iostream>
+using namespace std;
+int main(){
+	int r;
+	cout<<"Enter r value : ";
+	cin>>r;
+	cout<<"\nArea of a circle is using the formula 3.14 * r * r => "<<3.14*r*r;
+	return 0;
+}
