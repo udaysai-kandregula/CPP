d5720fa2593977ed20a5de027cd657fb2b411fd6
Udaysai <udaysai.kandregula@gmail.com>
2025-11-22 08:34:37 +0530

    Finging area of rectangle in cpp

---
+++ rectangleArea.cpp
@@ -0,0 +1,13 @@
+//Area of a rectangle if  length and breath are given run time
+
+#include<iostream>
+using namespace std;
+int main(){
+	int l,b;
+	cout<<"Enter length : ";
+	cin>>l;
+	cout<<"\nEnter breath : ";
+	cin>>b;
+	cout<<"\nArea of a rectangle is using (l*b) "<<l*b;
+	return 0;
+}
