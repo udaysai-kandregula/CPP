3e911f30e677d3402b5919a08cb7f0d0436639c7
Udaysai <udaysai.kandregula@gmail.com>
2025-11-24 12:45:19 +0530

    converting char into ASCII value

---
+++ ASCII.cpp
@@ -0,0 +1,25 @@
+//program to find ASCII values of a given character 
+
+//input= 'A'
+//output=65 
+#include<iostream>
+using namespace std;
+int main(){
+	char ch ;
+	cout<<"Enter your input : ";
+	cin>>ch;
+	cout<<endl<<(int)ch;   //type conversion
+	return 0;
+}
+
+
+//logic 
+//iam taking the input from the user and
+//convert it into a number based on ASCII number
+
+//code output:
+//Enter your input : A
+//65
+//--------------------------------
+//Process exited after 1.995 seconds with return value 0
+
